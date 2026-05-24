a0c2a26c6481b3db69e4096a83ba431690618148
jyx-07 <s25069@gsm.hs.kr>
2026-05-24 18:29:44 +0900

    init

---
+++ src/main/kotlin/team/darkmoderap/aikon/AikonApplication.kt
@@ -0,0 +1,11 @@
+package team.darkmoderap.aikon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AikonApplication
+
+fun main(args: Array<String>) {
+	runApplication<AikonApplication>(*args)
+}
---
+++ src/test/kotlin/team/darkmoderap/aikon/AikonApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.darkmoderap.aikon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AikonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
