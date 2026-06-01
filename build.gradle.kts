06916796fffb628237aca323da6465b7906b9ad5
jyx-07 <s25069@gsm.hs.kr>
2026-06-02 00:54:04 +0900

    add :: 헬스체크 컨트롤러 및 Swagger 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("software.amazon.awssdk:s3")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
---
+++ src/main/kotlin/team/darkmoderap/aikon/global/config/SwaggerConfig.kt
@@ -0,0 +1,19 @@
+package team.darkmoderap.aikon.global.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun openAPI(): OpenAPI =
+        OpenAPI()
+            .info(
+                Info()
+                    .title("AIKON API")
+                    .description("AIKON REST API Documentation")
+                    .version("v1.0"),
+            )
+}
