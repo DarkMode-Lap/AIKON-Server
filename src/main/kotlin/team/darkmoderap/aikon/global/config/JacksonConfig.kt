913c75395ce8c1534c096231dc65156dbfa79096
hamin <hamin090403@gmail.com>
2026-06-04 15:02:54 +0900

    fix :: JavaTimeModule 및 KotlinModule 명시적 등록

--- src/main/kotlin/team/darkmoderap/aikon/global/config/JacksonConfig.kt
+++ src/main/kotlin/team/darkmoderap/aikon/global/config/JacksonConfig.kt
@@ -1,6 +1,8 @@
 package team.darkmoderap.aikon.global.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,5 +10,9 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class JacksonConfig {
     @Bean
-    fun objectMapper(): ObjectMapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
+    fun objectMapper(): ObjectMapper =
+        ObjectMapper()
+            .registerModule(KotlinModule.Builder().build())
+            .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 }
